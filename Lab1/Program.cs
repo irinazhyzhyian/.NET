71b910757d38de6a5e87853e12ddf130183fbb1c
Irina Zhyzhyian <gygyjan.iryna@gmail.com>
2020-09-04 22:58:18 +0300

    Created connection string for Postgres

---
+++ Lab1/Program.cs
@@ -0,0 +1,49 @@
+﻿using Npgsql;
+using System;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                NpgsqlConnectionStringBuilder builder = new NpgsqlConnectionStringBuilder();
+
+                builder.ConnectionString = "Server=127.0.0.1;Port=5432;User Id=postgres;Password=123456;Database=Pharmacy;";
+
+                using (NpgsqlConnection connection = new NpgsqlConnection(builder.ConnectionString))
+                {
+                    Console.WriteLine("\nQuery data example:");
+                    Console.WriteLine("=========================================\n");
+
+                    connection.Open();
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("select * ");
+                    sb.Append("from medicines ");
+                    String sql = sb.ToString();
+
+                    using (NpgsqlCommand command = new NpgsqlCommand(sql, connection))
+                    {
+                        using (NpgsqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0} {1}", reader.GetInt32(0).ToString(), reader.GetString(1));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (NpgsqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("\nDone. Press enter.");
+            Console.ReadLine();
+        }
+    }
+}
